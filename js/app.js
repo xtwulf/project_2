654504732e006908ddb865b9a091fe5e89a7dd8b
Ulf Wilke <ulf-wilke@web.de>
2021-03-12 09:07:36 +0100

    scrolling refactored

--- js/app.js
+++ js/app.js
@@ -22,8 +22,12 @@
 const menu_elements = document.querySelectorAll('section');
 
 var menu_element_ids = [];
-
+var menu_element_pos = [];
 let debug_mode = false;
+
+let scrollOptions;
+
+let correction_y = 200;
 
 
 
@@ -34,10 +38,35 @@ let debug_mode = false;
  * 
 */
 
-function scrollTo(pos) 
-{
-    console.log("Scroll to element:" + pos);
-    window.scrollTo(0,pos);
+function getCoords(i) {
+    console.log("section"+i+" ",elements[i].getBoundingClientRect().top);
+    el = elements[i].getBoundingClientRect().top;
+    // window.scrollTo(0,0);
+
+    window.scrollTo(0,el-pageYOffset+correction_y);
+  
+    // console.log((box.top + pageYOffset) + " ", box.left + pageXOffset);
+  }
+
+
+function scrollToElement(i) {
+    y_pos = menu_element_pos[i] - getNavHeight();
+    scrollOptions = {
+        left: 0,
+        top: y_pos,
+        behavior: 'smooth'
+      }
+    window.scrollTo(scrollOptions);
+    console.log("scroll to y:",y_pos);   
+    console.log(menu_element_pos);
+    console.log("Nav-Height: ", getNavHeight());
+}
+
+function getNavHeight() {
+    var el = document.getElementById('navigation');
+    var nav_height = el.getBoundingClientRect().height;
+
+    return (nav_height);
 
 }
 
@@ -69,23 +98,17 @@ for (let i=0; i < menu_elements.length; i++) {
     node.classList.add('menu__link');
     node.setAttribute('id', 'nav'+i);
 
-    //apply on-click event on li-element for call the scroll function
-    let rect = menu_elements[i].getBoundingClientRect();
-    var y_pos = rect.top;
-      
-    node.setAttribute('onclick', `scrollTo(${y_pos})`);
+    // getting the section y-ccordinates and write them into array  
+    menu_element_pos[i] = menu_elements[i].getBoundingClientRect().top + pageYOffset;
+    
+    //apply on-click event on li-element for call the scroll function      
+    node.setAttribute('onclick', 'scrollToElement('+i+')');
 
 
     
     menu.appendChild(node);
     
     menu_element_ids.push('section' + i);
-
- 
-    
-
-
-
 }
 
 
@@ -94,18 +117,6 @@ for (let i=0; i < menu_elements.length; i++) {
 // ======================================================= //
 
 
-// Log coordinates in Frontend
-if (debug_mode) {
-    document.getElementById("#debug_area").style.display = "inline-flex";
-}
-
-else {document.getElementById("#debug_area").style.display = "none";}
-
-function showCoordinates (element) {
-    var rect = element.getBoundingClientRect();
-    var y_id = document.getElementById("#y1");
-    y_id.innerHTML = rect.top;
-}
 
 // Check if element is in viewport
 function isElementInViewport(element) {
@@ -118,28 +129,30 @@ function isElementInViewport(element) {
     );
    }
    
-   var elements = document.querySelectorAll(".section");
+var elements = document.querySelectorAll(".section");
     
-   function callbackFunc() {
-    for (var i = 0; i < elements.length; i++) {
-     if (isElementInViewport(elements[i])) {
-        elements[i].classList.add("visible");
-        document.getElementById('nav'+i).classList.add('visible')
-       }
-       
-    
-   
-    // Else-statement for clearing the .visible if element leaves viewport
-     else {
-         document.getElementById('nav' + i).classList.remove('visible');
-         elements[i].classList.remove("visible");
-     }
-     showCoordinates(elements[i]);
+function callbackFunc() {
+for (var i = 0; i < elements.length; i++) {
+    if (isElementInViewport(elements[i])) {
+    elements[i].classList.add("visible");
+    document.getElementById('nav'+i).classList.add('visible')
     }
-   }
     
-   window.addEventListener("load", callbackFunc);
-   window.addEventListener("scroll", callbackFunc);
+    // Else-statement for clearing the .visible if element leaves viewport
+    else {
+        document.getElementById('nav' + i).classList.remove('visible');
+        elements[i].classList.remove("visible");
+    }
+
+    if (debug_mode) {
+        var rect = elements[i].getBoundingClientRect();
+        console.log(`Section ${i}:`, rect.top);
+        }
+    }
+}
+    
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
 
 
 // Scroll to anchor ID using scrollTO event
