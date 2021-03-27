64d9e84a5c16d14f92d91388438ac7aad1fa7abf
Ulf Wilke <ulf-wilke@web.de>
2021-03-27 08:34:44 +0100

    added an active class on menu elements - when the section is in the viewport the related menu element gets an   active class - when leaving the viewport the class will be removed

--- js/app.js
+++ js/app.js
@@ -22,12 +22,11 @@
 const menu = document.getElementById('navbar__list');
 const menu_elements = document.querySelectorAll('section');
 
-
 let menu_element_pos = [];
 let debug_mode = false;
 let scrollOptions;
 let correction_y = 200;
-
+let navbar = document.getElementById('page__header');
 let elements = document.querySelectorAll(".section");
 
 
@@ -63,11 +62,53 @@ function getNavHeight() {
 
 function isElementInViewport(element) {
     let rect = element.getBoundingClientRect();
-
     return (
         rect.top >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight));
 }
+
+function hide_menu() {
+    // hide Navbar when scrolling, show it after timeout 
+    console.log("scrolling hide menu");
+    // navbar.classList.remove('page__header');
+
+/* 
+    setTimeout(function () {
+        navbar.classList.add('page__header');
+        
+    }, 1000); */
+
+    // fadeIn(navbar);
+}
+
+function fadeIn(element) {
+    setTimeout()
+    var opacity = 0;
+    var intervalID = setInterval(function() {
+
+        if (opacity < 1) {
+            opacity = opacity + 0.05
+            element.style.opacity = opacity;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 10);
+
+    element.classList.add('page__header');
+}
+
+function show_menu() {
+    navbar.classList.add('page__header');
+}
+    
+
+//Slide function 2
+function slide() {
+    var slideSource = document.getElementById('slideSource');
+    slideSource.classList.toggle('fade');
+}
+
+
 
 /**
  * End Helper Functions
@@ -77,14 +118,12 @@ function isElementInViewport(element) {
 
 
 
-// ======================================== //
-// Adding the style class to the ul Element //
-// ======================================== //
+// =============================================== //
+// Building the menu and doing other useful things //
+// =============================================== //
+
 
 menu.classList.add('navbar__menu');
-
-
-
 
 for (let i=0; i < menu_elements.length; i++) {
     // create LI nodes //
@@ -97,28 +136,34 @@ for (let i=0; i < menu_elements.length; i++) {
     node.classList.add('menu__link');
     node.setAttribute('id', 'nav'+i);
 
-    // getting the section y-ccordinates and write them into array  
+    // getting the section y-ccordinates and write them into array
+    // used for the scroll effect  
     menu_element_pos[i] = menu_elements[i].getBoundingClientRect().top + pageYOffset;
     
     //apply on-click event on li-element for call the scroll function      
     node.setAttribute('onclick', 'scrollToElement('+i+')');
 }
 
-// ======================================================= //
-// Add class 'active' to section when near top of viewport //
-// ======================================================= //
+// ======================================================================= //
+// Add class 'active' to section an nav elements when near top of viewport //
+// ======================================================================= //
 
        
 function callbackFunc() {
-    for (let i = 0; i < elements.length; i++) {
-        if (isElementInViewport(elements[i])) {
-            elements[i].classList.add("your-active-class");        
-        }
+    console.log("callbackFunc");
     
-    // Else-statement for clearing the .visible if element leaves viewport
-    else {
-        document.getElementById('nav' + i).classList.remove('visible');
-        elements[i].classList.remove("your-active-class");
+    for (let i = 0; i < elements.length; i++) {
+        let active_nav = "nav"+i;
+        if (isElementInViewport(elements[i])) {
+            elements[i].classList.add("your-active-class");
+            document.getElementById(active_nav).classList.add('nav_element_active');
+        }
+        
+        // Else-statement for clearing the .visible if element leaves viewport
+        else {
+            document.getElementById('nav' + i).classList.remove('visible');
+            elements[i].classList.remove("your-active-class");
+            document.getElementById(active_nav).classList.remove('nav_element_active');            
         }
 
     if (debug_mode) {
@@ -131,4 +176,7 @@ function callbackFunc() {
 }
     
 window.addEventListener("load", callbackFunc);
+//window.addEventListener("load", show_menu);
 window.addEventListener("scroll", callbackFunc);
+window.addEventListener("scroll", hide_menu);
+
